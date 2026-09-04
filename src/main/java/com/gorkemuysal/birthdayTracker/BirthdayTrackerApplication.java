fba5e650bbc29b02303b67c8c0e3926a61d5701c
grkmuysl <gorkmuysal@gmail.com>
2026-09-04 10:23:24 +0300

    Initial commit

---
+++ src/main/java/com/gorkemuysal/birthdayTracker/BirthdayTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.gorkemuysal.birthdayTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayTrackerApplication.class, args);
+	}
+
+}
