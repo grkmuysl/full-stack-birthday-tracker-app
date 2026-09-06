ef11550f6a27dd2c4cd6d6f9cab7e320cfd2a981
grkmuysl <gorkmuysal@gmail.com>
2026-09-06 13:22:53 +0300

    General Fixes
    To solve build error I made these fixes:
    1- I rewrite the compose.yaml file to docker builds
    2- I added new dependecies into pom.xml
    3- I created 2 new migration file to flyway (create tables)
    4- I fixed name conflict in the global exception handler

--- src/main/java/com/gorkemuysal/birthdayTracker/identity/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/gorkemuysal/birthdayTracker/identity/security/CustomAuthenticationEntryPoint.java
@@ -8,11 +8,11 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import tools.jackson.databind.ObjectMapper;
 
 /*
  * Custom Authentication Entry Point class implements AuthenticationEntryPoint
---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    role_name VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
