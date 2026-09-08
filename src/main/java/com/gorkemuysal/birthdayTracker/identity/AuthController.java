042213185e449ebfd403699a59cf1ef5296a1620
grkmuysl <gorkmuysal@gmail.com>
2026-09-08 11:28:32 +0300

    Import fixes and AuthenticationPrincipal  fixes
    1- I fixed the RequestBody imports in the AuthController.java , CategoryController and PersonController
    2- I fixed CustomUserDetails  import in the CategoryController and PersonController classes. Before this fix I coudn't get user from User Detail Service

--- src/main/java/com/gorkemuysal/birthdayTracker/identity/AuthController.java
+++ src/main/java/com/gorkemuysal/birthdayTracker/identity/AuthController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,6 @@ import com.gorkemuysal.birthdayTracker.identity.dto.RegisterRequest;
 import com.gorkemuysal.birthdayTracker.identity.dto.TokenResponse;
 import com.gorkemuysal.birthdayTracker.identity.security.CustomUserDetails;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
