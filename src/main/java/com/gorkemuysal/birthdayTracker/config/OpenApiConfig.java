edf05c918279d7c7b9bdf09750af0567565e115a
grkmuysl <gorkmuysal@gmail.com>
2026-09-08 10:43:04 +0300

    Create Confguration File
    I created new config file which name is OpenApiConfig. For the configuration Swagger UI. At the same time I added Autorize button to swagger UI

---
+++ src/main/java/com/gorkemuysal/birthdayTracker/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package com.gorkemuysal.birthdayTracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+// A configuration class for the Swagger
+@Configuration
+public class OpenApiConfig {
+	
+	/**
+	 * Thanks to this Bean I added Authorize button to swagger
+	 * */
+	@Bean
+	public OpenAPI customOpenAPI() {
+	    final String securitySchemeName = "bearerAuth";
+	    return new OpenAPI()
+	            .info(new Info().title("Birthday Tracker").version("v1.0.0"))
+	            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+	            .components(new Components()
+	                    .addSecuritySchemes(securitySchemeName,
+	                            new SecurityScheme()
+	                                    .name(securitySchemeName)
+	                                    .type(SecurityScheme.Type.HTTP)
+	                                    .scheme("bearer")
+	                                    .bearerFormat("JWT")));
+	}
+}
